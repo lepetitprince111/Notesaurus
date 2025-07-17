a742c4e310614b4dd5a0edc1bdf9a4ba427c9807
Dr0u1n <142617950+lepetitprince111@users.noreply.github.com>
2025-07-17 15:35:03 -0400

    first push

---
+++ Notesaurus/Notesaurus/Model/Classes.cs
@@ -0,0 +1,22 @@
+﻿using Notesaurus.FuturFonc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notesaurus.Model
+{
+    public class Classes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public string NbGroup { get; set; }
+        public IList<Notes> Note { get; set; } = new List<Notes>();
+        public IList<Grades> Notes { get; set; } = new List<Grades>();
+
+        public IList<string> Section { get; set; } = new List<string>();
+
+    }
+}
---
+++ Notesaurus/Notesaurus/Model/Semester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notesaurus.Model
+{
+    public class Semester
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Classes> SemesterClasses { get; set; } = new List<Classes>();
+    }
+}
